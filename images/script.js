537dd1abfde9d5c649f697572ddb737bfffa1dcf
은배 <geb9211@gmail.com>
2022-09-30 16:16:33 +0900

    ✨ Feat: 사이드바 토글 구현 및 구조 작성

---
+++ images/script.js
@@ -0,0 +1,7 @@
+const sidebar = document.getElementById("sidebar");
+function openSidebar() {
+  sidebar.setAttribute("style", "display: block");
+}
+function closeSidebar() {
+  sidebar.setAttribute("style", "display: none");
+}
