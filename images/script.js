b2f3b6ed794535c70ef07f79d47426e2f4ea070c
은배 <geb9211@gmail.com>
2022-10-03 22:53:57 +0900

    ✨ Feat: 좌측 메뉴바 애니메이션 추가

--- images/script.js
+++ images/script.js
@@ -1,7 +1,17 @@
 const sidebar = document.getElementById("sidebar");
 function openSidebar() {
-  sidebar.setAttribute("style", "display: block");
+  sidebar.classList.remove('-translate-x-80');
+  sidebar.classList.add('translate-x-0');
 }
 function closeSidebar() {
-  sidebar.setAttribute("style", "display: none");
+  sidebar.classList.remove('translate-x-0');
+  sidebar.classList.add('-translate-x-80');
 }
+
+// const searchBtn = document.getElementById("searchBtn");
+// const searchInput = document.getElementById("searchInput");
+
+// function hoverSearch() {
+//   searchBtn.searchBtn("style", "display: none");
+//   searchInput.setAttribute("style", "display: flex");
+// }
