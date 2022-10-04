d4afea1e09648bb1b6d1575e778fa7e5ae3d8f78
은배 <geb9211@gmail.com>
2022-10-04 23:22:36 +0900

    🚧 Feat: 코드블럭 디자인 작성 및 관련 로직 추가

--- images/script.js
+++ images/script.js
@@ -1,3 +1,25 @@
+const mainBanner = document.getElementById("main_banner");
+if (window.location.pathname !== "/" || window.location.pathname !== "/category") {
+  mainBanner.classList.add("hidden");
+  return;
+}
+
+const codeBlocks = document.getElementsByClassName("hljs");
+const codeTopBar = `
+  <div class="absolute flex top-2 left-2">
+    <div class="code_red code_dot"></div>
+    <div class="code_yellow code_dot"></div>
+    <div class="code_green code_dot"></div>
+  </div>
+`;
+
+console.log(codeBlocks.length);
+console.log('hi');
+for(const codeBlock of codeWrappers) {
+  const codes = codeBlock.innerHTML;
+  codeBlock.innerHTML = codeTopBar + codes;
+}
+
 const sidebar = document.getElementById("sidebar");
 function openSidebar() {
   sidebar.classList.remove('-translate-x-80');
@@ -15,3 +37,13 @@ function closeSidebar() {
 //   searchBtn.searchBtn("style", "display: none");
 //   searchInput.setAttribute("style", "display: flex");
 // }
+
+function onClickWriteBtn() {
+  const hostname = window.location.hostname;
+  window.location = hostname + "/manage/post";
+}
+
+function onClickSettingBtn() {
+  const hostname = window.location.hostname;
+  window.location = hostname + "/manage";
+}
